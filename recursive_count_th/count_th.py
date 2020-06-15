ae006c187c42683d4bc61732cc5bc3643c7b5cbd
CScori <49799016+CScori@users.noreply.github.com>
2020-06-14 20:17:51 -0700

    adds count passing tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,18 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+count = 0
 def count_th(word):
+    global count
     
-    # TBC
+    #dealing with a 0 case
+    if "th" not in word:
+        total = count
+        count = 0
+        return total
     
-    pass
+    #recursive call
+    elif "th" in word:
+        count += 1 
+        
+        return count_th("0" + word[:word.index('th')] + '0' + word[word.index('th') + 2 :])
